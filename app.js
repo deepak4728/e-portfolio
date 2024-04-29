4d2085fa5e5f6765c8d9e33e809cf408a78cd054
Deepak Yadav <130702851+deepak4728@users.noreply.github.com>
2024-04-30 00:43:04 +0530

    dynamic layout change

--- app.js
+++ app.js
@@ -1,33 +1,30 @@
-// let h3=document.querySelector('.section1 h3');
-// let name=h3.innerText;
-// console.log(name);
-// let dis='';
-
-// for(let i=0;i<name.length;i++){
-//     dis=dis+name[i];
-    
-//     setInterval(()=>{
-//         h3.innerText=dis;
-//         console.log("hi");
-//     },2000);
-// }
-
 // code for updating profile summary dynamically
 let summary=document.querySelector("#summary");
 summary.innerHTML="As a second-year BE student in AI & Data Science, I’m exploring Generative AI and Prompt Engineering. I aim to integrate AI models with Web Development, fueled by my passion for Data Science and creative problem-solving.";
 
-// code for updating skills dynamically
-let skillsList=document.querySelector("#skillsList");
-let skills=["<b>Languages:</b> C, C++, Python, JavaScript","<b>Development:</b> HTML, CSS, Bootstrap, Git, GitHub, Django","<b>Libraries:</b> Numpy, Pandas, Matplotlib","<b>Database:</b> MySQL","<b>Soft Skills:</b> Problem Solving & Critical Thinking, Leadership, Public Speaking, Writing, Time Management","Data Analytics, MS-Excel & Advanced Excel","Competitive programming with DSA and OOPS","Graphic Design- Adobe Photoshop"];
+// code for updaing certifications dynamically
 
-for(let i=0;i<skills.length;i++){
-    let li=document.createElement("li");
-    li.innerHTML=skills[i];
-    skillsList.appendChild(li);
+let certStack=document.querySelector("#certStack");
+let certList=[["https://drive.google.com/file/d/10YwhjAWfkhyOvdjATsz2nfKyubacnEsQ/view?usp=sharing","excelda.jpg","Excel Fundamentals for Data Analysis | Coursera"],["https://drive.google.com/file/d/1-_g5k0iCaGBqZY3ttg-GDvQ5d3oTItme/view?usp=sharing","esrc.jpg","Embedded Systems and Robotics Club | MBM University"],["https://drive.google.com/file/d/16B1mRIpZQSQNmsS7is5Chbj6YpHRm_o4/view?usp=drive_link","iai.jpg","Introduction to Artificial Intelligence | Infosys"],["https://drive.google.com/file/d/1RlMKQbzz_9LzKA3u7u_qzy5n2VlJVq5R/view?usp=drive_link","ids.jpg","Introduction to Data Science | Infosys"],["https://drive.google.com/file/d/1rnbDx4jpSTWERVqfoWwDUhThJmtxJAqh/view?usp=drive_link","idl.jpg","Introduction to Deep Learning | Infosys"],["https://drive.google.com/file/d/16Yn1MhJjdMKqlyf_9f-WKc16aXIzddxm/view?usp=drive_link","inlp.jpg","Introduction to Natural Language Processing | Infosys"],["https://drive.google.com/file/d/18qWap5681E_go_RaUNobacC5HCzuwetq/view?usp=drive_link","icv.jpg","Introduction to Computer Vision | Infosys"]];
+for(let i=0;i<certList.length;i++){
+    let div=document.createElement("div");
+    div.classList.add("card")
+    div.innerHTML=`<a  href="${certList[i][0]}" target="_blank"><img class="certStackImg" src="static/certificates/${certList[i][1]}" alt="${certList[i][2]}"><p>${certList[i][2]}</p></a>`;
+    certStack.appendChild(div);
+}
+// code for updating skills TechStack dynamically
+let techStack=document.querySelector("#techStack");
+let skillSet=["c++.png","python.png","javaScript.png","mysql.png","html.png","css.png","bootstrap.png","vscode.png","git.png","github.png","excel.png","photoshop.png"];
+
+for(let i=0;i<skillSet.length;i++){
+    let img=document.createElement("img");
+    img.src=`static/icons/techStack/${skillSet[i]}`;
+    img.alt=skillSet[i];
+    techStack.appendChild(img);
 }
 
 // code for updaing education dynamically
-let educationList=document.querySelector("#educationList");
+let c=document.querySelector("#educationList");
 let education=["BE in AI & Data Science MBM University, Jodhpur: First Division (Nov 22 – July 23)","JNV, Khairthal, Alwar Class 12th (PCM + CS): First Division with Distinction (April 20–July 21)"];
 for(let i=0;i<education.length;i++){
     let li=document.createElement("li");
@@ -35,11 +32,3 @@ for(let i=0;i<education.length;i++){
     educationList.appendChild(li);
 }
 
-// code for updaing certifications dynamically
-let certificateList=document.querySelector("#certificateList");
-let certificate=["<a href='https://disha.mbm.ac.in/certificate/1423' target='_blank'>Embedded Systems and Robotics Club | MBM University</a>","<a href='https://coursera.org/verify/CM7JMTAEVAKA'>Excel Fundamentals for Data Analysis Certificate</a>","Introduction to Artificial Intelligence | Infosys Springboard","Introduction to Data Science | Infosys Springboard","Introduction to Natural Language Processing | Infosys Springboard","Introduction to Deep Learning | Infosys Springboard"];
-for(let i=0;i<certificate.length;i++){
-    let li=document.createElement("li");
-    li.innerHTML=certificate[i];
-    certificateList.appendChild(li);
-}

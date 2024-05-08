237d65d6fff3df638d8342dc918cde5f691dc281
Deepak Yadav <130702851+deepak4728@users.noreply.github.com>
2024-05-09 01:00:21 +0530

    projects update

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ summary.innerHTML="\"Unleashing the power of AI & Data Science, I'm a second-yea
 
 // projects section code
 let projectStack=document.querySelector("#projectStack");
-let projectList=[["https://deepak4728.github.io/TechVerseX/","TechVerseX.jpg","TechVerseX"]];
+let projectList=[["https://deepak4728.github.io/TechVerseX/","TechVerseX.png","<b><i>TechverseX</b></i> is a dynamic web platform that bridges the gap between aspiring students and valuable opportunities"],["https://github.com/deepak4728/StudyPods-v4.0/tree/main/SP0410","autoML.png","<b><i>AutoML</b></i> Simplifys Custom Model Creation. Data Profiling Highlights issues like missing values and outliers."]];
 for (let i=0; i<projectList.length; i++){
     let div=document.createElement("div");
     div.classList.add("card")
@@ -14,7 +14,7 @@ for (let i=0; i<projectList.length; i++){
 
 // code for updaing certifications dynamically
 let certStack=document.querySelector("#certStack");
-let certList=[["https://drive.google.com/file/d/10YwhjAWfkhyOvdjATsz2nfKyubacnEsQ/view?usp=sharing","excelda.jpg","Excel Fundamentals for Data Analysis | Coursera"],["https://drive.google.com/file/d/1-_g5k0iCaGBqZY3ttg-GDvQ5d3oTItme/view?usp=sharing","esrc.jpg","Embedded Systems and Robotics Club | MBM University"],["https://drive.google.com/file/d/16B1mRIpZQSQNmsS7is5Chbj6YpHRm_o4/view?usp=drive_link","iai.jpg","Introduction to Artificial Intelligence | Infosys"],["https://drive.google.com/file/d/1RlMKQbzz_9LzKA3u7u_qzy5n2VlJVq5R/view?usp=drive_link","ids.jpg","Introduction to Data Science | Infosys"],["https://drive.google.com/file/d/1rnbDx4jpSTWERVqfoWwDUhThJmtxJAqh/view?usp=drive_link","idl.jpg","Introduction to Deep Learning | Infosys"],["https://drive.google.com/file/d/16Yn1MhJjdMKqlyf_9f-WKc16aXIzddxm/view?usp=drive_link","inlp.jpg","Introduction to Natural Language Processing | Infosys"],["https://drive.google.com/file/d/18qWap5681E_go_RaUNobacC5HCzuwetq/view?usp=drive_link","icv.jpg","Introduction to Computer Vision | Infosys"]];
+let certList=[["https://drive.google.com/file/d/10YwhjAWfkhyOvdjATsz2nfKyubacnEsQ/view?usp=sharing","excelda.jpg","Excel Fundamentals for Data Analysis | Coursera"],["https://drive.google.com/file/d/1-_g5k0iCaGBqZY3ttg-GDvQ5d3oTItme/view?usp=sharing","esrc.jpg","Embedded Systems and Robotics Club | MBM University"],["https://drive.google.com/file/d/16B1mRIpZQSQNmsS7is5Chbj6YpHRm_o4/view?usp=drive_link","iai.jpg","Artificial Intelligence | Infosys"],["https://drive.google.com/file/d/1RlMKQbzz_9LzKA3u7u_qzy5n2VlJVq5R/view?usp=drive_link","ids.jpg","Data Science | Infosys"],["https://drive.google.com/file/d/1rnbDx4jpSTWERVqfoWwDUhThJmtxJAqh/view?usp=drive_link","idl.jpg","Deep Learning | Infosys"],["https://drive.google.com/file/d/16Yn1MhJjdMKqlyf_9f-WKc16aXIzddxm/view?usp=drive_link","inlp.jpg","Natural Language Processing | Infosys"],["https://drive.google.com/file/d/18qWap5681E_go_RaUNobacC5HCzuwetq/view?usp=drive_link","icv.jpg","Computer Vision | Infosys"]];
 for(let i=0;i<certList.length;i++){
     let div=document.createElement("div");
     div.classList.add("card")
